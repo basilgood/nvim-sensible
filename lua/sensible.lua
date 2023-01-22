dcb19e0f30167e9acd8eed4f8d9a9dcb1a54740a
basilgood <elsile69@yahoo.com.com>
2023-01-22 21:26:19 +0200

    feat: make it configurable

--- lua/sensible.lua
+++ lua/sensible.lua
@@ -1,31 +1,44 @@
-vim.opt.path = '.,**'
-vim.opt.swapfile = false
-vim.opt.undofile = true
-vim.opt.autowrite = true
-vim.opt.autowriteall = true
-vim.opt.gdefault = true
-vim.opt.number = true
-vim.opt.termguicolors = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.wrap = false
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.signcolumn = 'yes'
-vim.opt.updatetime = 200
-vim.opt.diffopt:append('context:3,indent-heuristic,algorithm:patience')
-vim.opt.shortmess:append('scI')
-vim.opt.showmode = false
-vim.opt.completeopt = 'menuone,noselect'
-vim.opt.list = true
-vim.opt.listchars = { tab = '🢝 ', lead = '·', trail = '·', nbsp = '␣' }
-vim.opt.sessionoptions = 'buffers,curdir,tabpages,folds,winpos,winsize'
-vim.opt.wildignore = { '*/node_modules/*,*/.git/*' }
-vim.opt.wildmode = 'longest:full,full'
-vim.opt.pumheight = 5
-vim.opt.grepprg = 'rg --vimgrep'
-vim.opt.grepformat = '%f:%l:%c:%m'
-vim.opt.laststatus = 2
-vim.opt.statusline = [[%{expand('%:p:h:t')}/%t%h%r%#error#%m%*%=[%{strlen(&ft)?&ft:'none'}]%*%4c:%l/%L]]
+local M = {}
+
+local defaults = {
+  path = '.,**',
+  swapfile = false,
+  undofile = true,
+  autowrite = true,
+  autowriteall = true,
+  gdefault = true,
+  number = true,
+  termguicolors = true,
+  tabstop = 2,
+  softtabstop = 2,
+  shiftwidth = 2,
+  expandtab = true,
+  wrap = false,
+  splitbelow = true,
+  splitright = true,
+  signcolumn = 'yes',
+  updatetime = 200,
+  diffopt = 'internal,filler,closeoff,context:3,indent-heuristic,algorithm:patience',
+  shortmess = 'tonfFOxTcsiIl',
+  showmode = false,
+  completeopt = 'menuone,noselect',
+  list = true,
+  listchars = { tab = '🢝 ', lead = '·', trail = '·', nbsp = '␣' },
+  sessionoptions = 'buffers,curdir,tabpages,folds,winpos,winsize',
+  wildignore = { '*/node_modules/*,*/.git/*' },
+  wildmode = 'longest:full,full',
+  pumheight = 5,
+  grepprg = 'rg --vimgrep',
+  grepformat = '%f:%l:%c:%m',
+  laststatus = 2,
+  statusline = [[%{expand('%:p:h:t')}/%t%h%r%#error#%m%*%=[%{strlen(&ft)?&ft:'none'}]%*%4c:%l/%L]],
+}
+
+function M.setup(opts)
+  local all = vim.tbl_deep_extend('force', defaults, opts)
+  for set, value in pairs(all) do
+    vim.opt[set] = value
+  end
+end
+
+return M
