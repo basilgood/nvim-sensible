fadc76fbc12301555a4516264dca339673e7f28f
basilgood <elsile69@yahoo.com.com>
2023-02-07 15:24:21 +0200

    chore: seperate global and opt

--- lua/sensible.lua
+++ lua/sensible.lua
@@ -1,43 +1,61 @@
 local M = {}
 
 local defaults = {
-  path = '.,**',
-  swapfile = false,
-  undofile = true,
-  autowrite = true,
-  autowriteall = true,
-  gdefault = true,
-  virtualedit = 'onemore',
-  number = true,
-  termguicolors = true,
-  tabstop = 2,
-  softtabstop = 2,
-  shiftwidth = 2,
-  expandtab = true,
-  wrap = false,
-  splitbelow = true,
-  splitright = true,
-  signcolumn = 'yes',
-  updatetime = 200,
-  diffopt = 'internal,filler,closeoff,context:3,indent-heuristic,algorithm:patience',
-  shortmess = 'tonfFOxTcsiIl',
-  showmode = false,
-  completeopt = 'menuone,noselect',
-  complete = '.,w,b,u,i',
-  list = true,
-  listchars = { tab = '🢝 ', lead = '·', trail = '·', nbsp = '␣', eol = '¬' },
-  sessionoptions = 'buffers,curdir,tabpages,folds,winpos,winsize',
-  wildignore = { '*/node_modules/*,*/.git/*' },
-  wildmode = 'longest:full,full',
-  pumheight = 5,
-  grepprg = 'rg --vimgrep',
-  grepformat = '%f:%l:%c:%m',
-  statusline = ' %{mode()} | %t %m %r %= %c,%l/%L    %y',
+  options = {
+    path = '.,**',
+    swapfile = false,
+    undofile = true,
+    autowrite = true,
+    autowriteall = true,
+    gdefault = true,
+    number = true,
+    termguicolors = true,
+    tabstop = 2,
+    softtabstop = 2,
+    shiftwidth = 2,
+    expandtab = true,
+    wrap = false,
+    splitbelow = true,
+    splitright = true,
+    signcolumn = 'yes',
+    updatetime = 200,
+    diffopt = 'internal,filler,closeoff,context:3,indent-heuristic,algorithm:patience',
+    shortmess = 'tonfFOxTcsiIl',
+    showmode = false,
+    completeopt = 'menuone,noselect',
+    complete = '.,w,b,u,i',
+    list = true,
+    listchars = { tab = '🢝 ', lead = '·', trail = '·', nbsp = '␣', eol = '¬' },
+    sessionoptions = 'buffers,curdir,tabpages,folds,winpos,winsize',
+    wildignore = { '*/node_modules/*,*/.git/*' },
+    wildmode = 'longest:full,full',
+    pumheight = 5,
+    grepprg = 'rg --vimgrep',
+    grepformat = '%f:%l:%c:%m',
+    statusline = ' %{mode()} | %t %m %r %= %c,%l/%L    %y',
+  },
+
+  global = {
+    netrw_list_hide = '^./$,^../$',
+    netrw_bufsettings = 'noma nomod nonu nobl nowrap ro nornu nocul',
+    netrw_banner = 0,
+    netrw_preview = 1,
+    netrw_alto = 'spr',
+    netrw_use_errorwindow = 0,
+    netrw_special_syntax = 1,
+    netrw_localcopydircmd = 'cp -r',
+  },
 }
 
+local addopts = {}
+
 function M.setup(addopts)
-  local opts = vim.tbl_deep_extend('force', defaults, addopts)
-  for set, value in pairs(opts) do
+  allopts = vim.tbl_deep_extend('force', defaults, addopts or {})
+  for set, value in pairs(allopts.global) do
+    vim.g[set] = value
+  end
+
+  for set, value in pairs(allopts.options) do
     vim.opt[set] = value
   end
 end
