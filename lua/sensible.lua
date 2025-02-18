0abb152a360098415fcb7e0c1d267f7e33cbabf8
vasy <elsile69@yahoo.com>
2025-02-18 21:09:56 +0200

    chore: update in a more functional programming

--- lua/sensible.lua
+++ lua/sensible.lua
@@ -1,7 +1,7 @@
 local M = {}
 
-local defaults = {
-  options = {
+local function get_options()
+  return {
     path = '.,**',
     swapfile = false,
     undofile = true,
@@ -38,9 +38,11 @@ local defaults = {
     grepprg = 'rg --color=never --vimgrep',
     grepformat = '%f:%l:%c:%m',
     statusline = ' %{expand("%:p:h:t")}/%t %{&modified?" ":""} %r %= %c:%l/%L    %y',
-  },
+  }
+end
 
-  global = {
+local function get_globals()
+  return {
     netrw_list_hide = [[^\.\/$,^\.\.\/$]],
     netrw_bufsettings = 'noma nomod nonu nobl nowrap ro nornu nocul scl=no',
     netrw_banner = 0,
@@ -50,25 +52,29 @@ local defaults = {
     netrw_special_syntax = 1,
     netrw_localcopydircmd = 'cp -r',
     netrw_localrmdir = 'rm -r',
-  },
-}
+  }
+end
+
+local function apply_global_opts(global_opts)
+  for key, value in pairs(global_opts) do
+    if value ~= nil then
+      vim.g[key] = value
+    end
+  end
+end
+
+local function apply_local_opts(local_opts)
+  for key, value in pairs(local_opts) do
+    if value ~= nil then
+      vim.opt[key] = value
+    end
+  end
+end
 
 function M.setup(addopts)
-  local opts = vim.tbl_deep_extend('force', defaults, addopts or {})
-  M._apply_global_opts(opts.global)
-  M._apply_local_opts(opts.options)
-end
-
-function M._apply_global_opts(global_opts)
-  for key, value in pairs(global_opts) do
-    vim.g[key] = value
-  end
-end
-
-function M._apply_local_opts(local_opts)
-  for key, value in pairs(local_opts) do
-    vim.opt[key] = value
-  end
+  local opts = vim.tbl_deep_extend('force', { options = get_options(), global = get_globals() }, addopts or {})
+  apply_global_opts(opts.global)
+  apply_local_opts(opts.options)
 end
 
 return M
